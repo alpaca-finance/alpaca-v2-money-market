e3f4f23a2cccd77e25ac30b67b52e4e0474ee3b8
sora-alpaca <sora.alpaca@gmail.com>
2022-11-02 14:49:06 +0700

    feat: support custom decimals

--- solidity/contracts/money-market/IbToken.sol
+++ solidity/contracts/money-market/IbToken.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -6,10 +7,14 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { IIbToken } from "./interfaces/IIbToken.sol";
 
 contract IbToken is ERC20, IIbToken {
+  uint8 private _decimals;
+
   constructor(
-    string memory name,
-    string memory symbol
-  ) public ERC20(name, symbol) {
+    string memory name_,
+    string memory symbol_,
+    uint8 decimals_
+  ) ERC20(name_, symbol_) {
+    _decimals = decimals_;
   }
 
   function mint(address to, uint256 amount) external {
@@ -18,5 +23,9 @@ contract IbToken is ERC20, IIbToken {
 
   function burn(address from, uint256 amount) external {
     _burn(from, amount);
+  }
+
+  function decimals() public view virtual override returns (uint8) {
+    return _decimals;
   }
 }
