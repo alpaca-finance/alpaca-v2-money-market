c1f87df0ab8a6faed3a1b3c43aaa3736aa8e0077
alpanaca <rblue4594@gmail.com>
2022-11-11 11:46:23 +0700

    fix: wrong assignment

--- solidity/contracts/money-market/libraries/LibMoneyMarket01.sol
+++ solidity/contracts/money-market/libraries/LibMoneyMarket01.sol
@@ -161,7 +161,7 @@ library LibMoneyMarket01 {
         moneyMarketDs.debtShares[_borrowed[_i].token]
       );
 
-      _totalUsedBorrowedPower = usedBorrowedPower(_borrowedAmount, _tokenPrice, _tokenConfig.borrowingFactor);
+      _totalUsedBorrowedPower += usedBorrowedPower(_borrowedAmount, _tokenPrice, _tokenConfig.borrowingFactor);
 
       unchecked {
         _i++;
