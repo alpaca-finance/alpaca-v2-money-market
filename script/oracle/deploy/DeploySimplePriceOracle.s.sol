65a01f9f96b881d13deec9e1443f76ee0901bd8a
abc-alpaca <gabcdef637@gmail.com>
2022-11-15 11:12:57 +0700

    Revert "chore: remove method have't done"
    This reverts commit 07e733a8f8c78a6b610f444aa817afd89d856e3e.

--- script/oracle/deploy/DeploySimplePriceOracle.s.sol
+++ script/oracle/deploy/DeploySimplePriceOracle.s.sol
@@ -12,6 +12,32 @@ contract MyScript is Script {
     _deployContract(vm.addr(deployerPrivateKey));
   }
 
+  /* TODOS
+1 impersonate as deployer (DONE)
+2 deploy upgradeable contract (TODO)
+3 writing mainnet.json
+ */
+  function _deployUpgradableContract(address deployer) internal {
+    address proxyAdmin = address(0x5379F32C8D5F663EACb61eeF63F722950294f452);
+    address alpacaDeployer = address(0xC44f82b07Ab3E691F826951a6E335E1bC1bB0B51);
+
+    if (alpacaDeployer == deployer) {
+      vm.startBroadcast(deployer);
+    } else {
+      vm.startBroadcast();
+    }
+
+    SimplePriceOracle simplePriceOracle = new SimplePriceOracle();
+    // simplePriceOracle.initialize(address(0x4eA137cc292bA6d046b2541D99472B65E60eF413));
+
+    bytes memory _data = abi.encode(address(0x4eA137cc292bA6d046b2541D99472B65E60eF413));
+
+    TransparentUpgradeableProxy tp = new TransparentUpgradeableProxy(address(simplePriceOracle), proxyAdmin, _data);
+    console.log("PROXY", address(tp));
+
+    vm.stopBroadcast();
+  }
+
   function _deployContract(address deployer) internal {
     address proxyAdmin = address(0x5379F32C8D5F663EACb61eeF63F722950294f452);
     address alpacaDeployer = address(0xC44f82b07Ab3E691F826951a6E335E1bC1bB0B51);
