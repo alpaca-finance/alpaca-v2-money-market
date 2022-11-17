56bfa7c615b145fd5fda10123e164df5600c64bf
alpanaca <rblue4594@gmail.com>
2022-11-17 17:05:00 +0700

    chore: change license

--- solidity/contracts/lyf/interfaces/IPancakeRouter02.sol
+++ solidity/contracts/lyf/interfaces/IPancakeRouter02.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: BUSL
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
 interface IPancakeRouter02 {
--- solidity/contracts/money-market/initializers/MoneyMarketInit.sol
+++ solidity/contracts/money-market/initializers/MoneyMarketInit.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL
 pragma solidity 0.8.17;
 
 import { LibMoneyMarket01 } from "../libraries/LibMoneyMarket01.sol";
