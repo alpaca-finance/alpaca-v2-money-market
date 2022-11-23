0bd4a901eb245e3efa2231f40d191cea43728e4c
sora-alpaca <sora.alpaca@gmail.com>
2022-11-23 17:50:55 +0700

    feat: add alpacaV2Oracle and medianizer

--- solidity/contracts/lyf/libraries/LibLYF01.sol
+++ solidity/contracts/lyf/libraries/LibLYF01.sol
@@ -10,7 +10,7 @@ import { LibShareUtil } from "./LibShareUtil.sol";
 import { LibShareUtil } from "../libraries/LibShareUtil.sol";
 
 // interfaces
-import { IPriceOracle } from "../interfaces/IPriceOracle.sol";
+import { IAlpacaV2Oracle } from "../interfaces/IAlpacaV2Oracle.sol";
 import { IIbToken } from "../interfaces/IIbToken.sol";
 import { IMoneyMarket } from "../interfaces/IMoneyMarket.sol";
 
@@ -43,7 +43,7 @@ library LibLYF01 {
   // Storage
   struct LYFDiamondStorage {
     address moneyMarket;
-    IPriceOracle oracle;
+    IAlpacaV2Oracle oracle;
     mapping(address => uint256) collats;
     mapping(address => LibDoublyLinkedList.List) subAccountCollats;
     mapping(address => TokenConfig) tokenConfigs;
@@ -153,10 +153,7 @@ library LibLYF01 {
   }
 
   function getPriceUSD(address _token, LYFDiamondStorage storage lyfDs) internal view returns (uint256, uint256) {
-    (uint256 _price, uint256 _lastUpdated) = lyfDs.oracle.getPrice(
-      _token,
-      address(0x115dffFFfffffffffFFFffffFFffFfFfFFFFfFff)
-    );
+    (uint256 _price, uint256 _lastUpdated) = lyfDs.oracle.getTokenPrice(_token);
     if (_lastUpdated < block.timestamp - lyfDs.tokenConfigs[_token].maxToleranceExpiredSecond)
       revert LibLYF01_PriceStale(_token);
     return (_price, _lastUpdated);
