5c5f2f08b6da93bd056c2a2c5641285bcdb83371
sora-alpaca <sora.alpaca@gmail.com>
2022-11-30 17:07:38 +0700

    feat: reward distributor add whitelisted

--- solidity/contracts/money-market/RewardDistributor.sol
+++ solidity/contracts/money-market/RewardDistributor.sol
@@ -10,13 +10,29 @@ import { IRewardDistributor } from "./interfaces/IRewardDistributor.sol";
 contract RewardDistributor is IRewardDistributor {
   using SafeERC20 for ERC20;
 
+  mapping(address => bool) public callersOk;
+
   function safeTransferReward(
     address _token,
     address _to,
     uint256 _amount
   ) external {
+    address _caller = msg.sender;
+    if (!callersOk[_caller]) revert RewardDistributor_Unauthorized(_caller);
     ERC20 _rewardToken = ERC20(_token);
     if (_rewardToken.balanceOf(address(this)) < _amount) revert RewardDistributor_InsufficientBalance(_token, _amount);
     _rewardToken.safeTransfer(_to, _amount);
+
+    emit LogSafeTransferReward(_token, _caller, _to, _amount);
+  }
+
+  function setCallerOk(address[] calldata _callers, bool isOk) external {
+    uint256 _length = _callers.length;
+    for (uint8 _i; _i < _length; ) {
+      callersOk[_callers[_i]] = isOk;
+      unchecked {
+        _i++;
+      }
+    }
   }
 }
--- solidity/contracts/money-market/interfaces/IRewardDistributor.sol
+++ solidity/contracts/money-market/interfaces/IRewardDistributor.sol
@@ -8,6 +8,10 @@ interface IRewardDistributor {
     uint256 _amount
   ) external;
 
+  // events
+  event LogSafeTransferReward(address indexed _token, address _from, address _to, uint256 _amount);
+
   // errors
   error RewardDistributor_InsufficientBalance(address _token, uint256 _amount);
+  error RewardDistributor_Unauthorized(address _caller);
 }
