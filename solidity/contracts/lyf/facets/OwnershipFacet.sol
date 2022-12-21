da78bba4a3ad17adf1a71f6bd3912ef2ccbc703a
69alpacas <>
2022-12-21 16:24:30 +0700

    feat: copy Ownership2Step

--- solidity/contracts/lyf/facets/OwnershipFacet.sol
+++ solidity/contracts/lyf/facets/OwnershipFacet.sol
@@ -5,21 +5,41 @@ import { LibDiamond } from "../libraries/LibDiamond.sol";
 import { IERC173 } from "../interfaces/IERC173.sol";
 
 contract OwnershipFacet is IERC173 {
-  error OwnershipFacet_Unauthorized();
+  error OwnershipFacet_CallerIsNotPendingOwner();
 
-  address public pendingOwner;
+  event OwnershipTransferStarted(address indexed previousOwner, address indexed newOwner);
+
+  address private _pendingOwner;
 
   function transferOwnership(address _newOwner) external override {
     LibDiamond.enforceIsContractOwner();
-    pendingOwner = _newOwner;
+
+    _pendingOwner = _newOwner;
+
+    emit OwnershipTransferStarted(LibDiamond.contractOwner(), _newOwner);
   }
 
-  function claimOwnership() external {
-    if (msg.sender != pendingOwner) revert OwnershipFacet_Unauthorized();
-    LibDiamond.setContractOwner(pendingOwner);
+  function acceptOwnership() external {
+    if (msg.sender != _pendingOwner) revert OwnershipFacet_CallerIsNotPendingOwner();
+
+    address _previousOwner = LibDiamond.contractOwner();
+    LibDiamond.setContractOwner(_pendingOwner);
+    delete _pendingOwner;
+
+    emit OwnershipTransferred(_previousOwner, LibDiamond.contractOwner());
   }
 
+  /**
+   * @dev Returns the address of the current owner.
+   */
   function owner() external view override returns (address owner_) {
     owner_ = LibDiamond.contractOwner();
+  }
+
+  /**
+   * @dev Returns the address of the pending owner.
+   */
+  function pendingOwner() external view returns (address) {
+    return _pendingOwner;
   }
 }
