0f853c70299c09e996d5b03f7dc695d689920ea4
sora-alpaca <sora.alpaca@gmail.com>
2022-12-21 13:55:23 +0700

    test: miniFL add pool test cases

---
+++ solidity/tests/miniFL/MiniFLTest_AddPool.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import { IERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+
+import { MiniFL_BaseTest } from "./MiniFL_BaseTest.t.sol";
+
+// interfaces
+import { IMiniFL } from "../../contracts/miniFL/interfaces/IMiniFL.sol";
+import { IRewarder } from "../../contracts/miniFL/interfaces/IRewarder.sol";
+
+contract MiniFLTest_AddPool is MiniFL_BaseTest {
+  function setUp() public override {
+    super.setUp();
+  }
+
+  function testCorrectness_WhenAddPool() external {
+    assertEq(miniFL.poolLength(), 0);
+    assertEq(miniFL.totalAllocPoint(), 0);
+
+    miniFL.addPool(100, IERC20Upgradeable(address(weth)), false, false);
+    miniFL.addPool(50, IERC20Upgradeable(address(usdc)), false, false);
+
+    assertEq(miniFL.poolLength(), 2);
+    assertEq(miniFL.totalAllocPoint(), 150);
+  }
+
+  function testRevert_WhenAddSuplicatedStakingTokenPool() external {
+    miniFL.addPool(100, IERC20Upgradeable(address(weth)), false, false);
+    vm.expectRevert(abi.encodeWithSelector(IMiniFL.MiniFL_DuplicatePool.selector));
+    miniFL.addPool(100, IERC20Upgradeable(address(weth)), false, false);
+  }
+}
