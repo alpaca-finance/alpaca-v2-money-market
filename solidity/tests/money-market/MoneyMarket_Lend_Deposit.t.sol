7d7bb1b87129b07fb39c93dd90829d052257919b
jr-alpaca <uviveramrom@gmail.com>
2022-12-21 16:36:00 +0700

    chore: refactor MM lend Facet test

---
+++ solidity/tests/money-market/MoneyMarket_Lend_Deposit.t.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import { MoneyMarket_BaseTest } from "./MoneyMarket_BaseTest.t.sol";
+
+// libs
+import { LibMoneyMarket01 } from "../../contracts/money-market/libraries/LibMoneyMarket01.sol";
+
+// interfaces
+import { ILendFacet } from "../../contracts/money-market/facets/LendFacet.sol";
+import { IERC20 } from "../../contracts/money-market/interfaces/IERC20.sol";
+
+contract MoneyMarket_Lend_Deposit_Test is MoneyMarket_BaseTest {
+  function setUp() public override {
+    super.setUp();
+  }
+
+  function testCorrectness_WhenUserDeposit_TokenShouldSafeTransferFromUserToMM() external {
+    vm.startPrank(ALICE);
+    weth.approve(moneyMarketDiamond, 10 ether);
+    lendFacet.deposit(address(weth), 10 ether);
+    vm.stopPrank();
+
+    assertEq(weth.balanceOf(ALICE), 990 ether);
+    assertEq(weth.balanceOf(moneyMarketDiamond), 10 ether);
+
+    assertEq(ibWeth.balanceOf(ALICE), 10 ether);
+  }
+
+  function testCorrectness_WhenMultipleDeposit_ShouldMintShareCorrectly() external {
+    uint256 _depositAmount1 = 10 ether;
+    uint256 _depositAmount2 = 20 ether;
+    uint256 _expectedTotalShare = 0;
+
+    vm.startPrank(ALICE);
+    weth.approve(moneyMarketDiamond, _depositAmount1);
+    lendFacet.deposit(address(weth), _depositAmount1);
+    vm.stopPrank();
+
+    // frist deposit mintShare = depositAmount
+    _expectedTotalShare += _depositAmount1;
+    assertEq(ibWeth.balanceOf(ALICE), _depositAmount1);
+
+    weth.mint(BOB, _depositAmount2);
+    vm.startPrank(BOB);
+    weth.approve(moneyMarketDiamond, _depositAmount2);
+    lendFacet.deposit(address(weth), _depositAmount2);
+    vm.stopPrank();
+
+    // mintShare = 20 * 10 / 10 = 20
+    uint256 _expectedBoBShare = 20 ether;
+    _expectedTotalShare += _expectedBoBShare;
+    assertEq(ibWeth.balanceOf(BOB), 20 ether);
+    assertEq(ibWeth.totalSupply(), _expectedTotalShare);
+  }
+
+  function testRevert_WhenUserDepositInvalidToken_ShouldRevert() external {
+    address _randomToken = address(10);
+    vm.startPrank(ALICE);
+    vm.expectRevert(abi.encodeWithSelector(ILendFacet.LendFacet_InvalidToken.selector, _randomToken));
+    lendFacet.deposit(_randomToken, 10 ether);
+    vm.stopPrank();
+  }
+
+  function testRevert_WhenUserDepositAndGetTinyShares_ShouldRevert() external {
+    vm.prank(ALICE);
+    vm.expectRevert(abi.encodeWithSelector(ILendFacet.LendFacet_NoTinyShares.selector));
+    lendFacet.deposit(address(weth), 1);
+  }
+
+  function testRevert_WhenUserWithdrawAndLeftWithTinyShares_ShouldRevert() external {
+    vm.startPrank(ALICE);
+    lendFacet.deposit(address(weth), 1 ether);
+    vm.expectRevert(abi.encodeWithSelector(LibMoneyMarket01.LibMoneyMarket01_NoTinyShares.selector));
+    lendFacet.withdraw(address(ibWeth), 0.5 ether);
+  }
+
+  function testCorrectness_WhenUserDepositETH_TokenShouldSafeTransferFromUserToMM() external {
+    vm.prank(ALICE);
+    lendFacet.depositETH{ value: 10 ether }();
+
+    assertEq(nativeToken.balanceOf(ALICE), 0 ether);
+    assertEq(ALICE.balance, 990 ether);
+    assertEq(nativeToken.balanceOf(moneyMarketDiamond), 10 ether);
+
+    assertEq(ibWNative.balanceOf(ALICE), 10 ether);
+  }
+}
--- solidity/tests/money-market/MoneyMarket_Lend_Withdraw.t.sol
+++ solidity/tests/money-market/MoneyMarket_Lend_Withdraw.t.sol
@@ -10,56 +10,9 @@ import { LibMoneyMarket01 } from "../../contracts/money-market/libraries/LibMone
 import { ILendFacet } from "../../contracts/money-market/facets/LendFacet.sol";
 import { IERC20 } from "../../contracts/money-market/interfaces/IERC20.sol";
 
-contract MoneyMarket_LendFacetTest is MoneyMarket_BaseTest {
+contract MoneyMarket_Lend_Withdraw_Test is MoneyMarket_BaseTest {
   function setUp() public override {
     super.setUp();
-  }
-
-  function testCorrectness_WhenUserDeposit_TokenShouldSafeTransferFromUserToMM() external {
-    vm.startPrank(ALICE);
-    weth.approve(moneyMarketDiamond, 10 ether);
-    lendFacet.deposit(address(weth), 10 ether);
-    vm.stopPrank();
-
-    assertEq(weth.balanceOf(ALICE), 990 ether);
-    assertEq(weth.balanceOf(moneyMarketDiamond), 10 ether);
-
-    assertEq(ibWeth.balanceOf(ALICE), 10 ether);
-  }
-
-  function testCorrectness_WhenMultipleDeposit_ShouldMintShareCorrectly() external {
-    uint256 _depositAmount1 = 10 ether;
-    uint256 _depositAmount2 = 20 ether;
-    uint256 _expectedTotalShare = 0;
-
-    vm.startPrank(ALICE);
-    weth.approve(moneyMarketDiamond, _depositAmount1);
-    lendFacet.deposit(address(weth), _depositAmount1);
-    vm.stopPrank();
-
-    // frist deposit mintShare = depositAmount
-    _expectedTotalShare += _depositAmount1;
-    assertEq(ibWeth.balanceOf(ALICE), _depositAmount1);
-
-    weth.mint(BOB, _depositAmount2);
-    vm.startPrank(BOB);
-    weth.approve(moneyMarketDiamond, _depositAmount2);
-    lendFacet.deposit(address(weth), _depositAmount2);
-    vm.stopPrank();
-
-    // mintShare = 20 * 10 / 10 = 20
-    uint256 _expectedBoBShare = 20 ether;
-    _expectedTotalShare += _expectedBoBShare;
-    assertEq(ibWeth.balanceOf(BOB), 20 ether);
-    assertEq(ibWeth.totalSupply(), _expectedTotalShare);
-  }
-
-  function testRevert_WhenUserDepositInvalidToken_ShouldRevert() external {
-    address _randomToken = address(10);
-    vm.startPrank(ALICE);
-    vm.expectRevert(abi.encodeWithSelector(ILendFacet.LendFacet_InvalidToken.selector, _randomToken));
-    lendFacet.deposit(_randomToken, 10 ether);
-    vm.stopPrank();
   }
 
   function testCorrectness_WhenUserWithdraw_ibTokenShouldBurnedAndTransferTokenToUser() external {
@@ -138,45 +91,11 @@ contract MoneyMarket_LendFacetTest is MoneyMarket_BaseTest {
     assertEq(ibWeth.totalSupply(), _expectedTotalShare);
   }
 
-  function testRevert_WhenUserDepositAndGetTinyShares_ShouldRevert() external {
-    vm.prank(ALICE);
-    vm.expectRevert(abi.encodeWithSelector(ILendFacet.LendFacet_NoTinyShares.selector));
-    lendFacet.deposit(address(weth), 1);
-  }
-
   function testRevert_WhenUserWithdrawAndLeftWithTinyShares_ShouldRevert() external {
     vm.startPrank(ALICE);
     lendFacet.deposit(address(weth), 1 ether);
     vm.expectRevert(abi.encodeWithSelector(LibMoneyMarket01.LibMoneyMarket01_NoTinyShares.selector));
     lendFacet.withdraw(address(ibWeth), 0.5 ether);
-  }
-
-  function testCorrectness_WhenUserOpenNewMarket_ShouldOpenOncePerToken() external {
-    vm.startPrank(ALICE);
-    // should pass when register new token
-    address _ibToken = lendFacet.openMarket(address(opm));
-    assertEq(IERC20(_ibToken).name(), "Interest Bearing OPM");
-    assertEq(IERC20(_ibToken).symbol(), "IBOPM");
-    assertEq(IERC20(_ibToken).decimals(), 9);
-
-    vm.expectRevert(abi.encodeWithSelector(ILendFacet.LendFacet_InvalidToken.selector, address(opm)));
-    lendFacet.openMarket(address(opm));
-
-    // able to deposit
-    lendFacet.deposit(address(opm), 5 ether);
-    assertEq(IERC20(_ibToken).balanceOf(ALICE), 5 ether);
-    vm.stopPrank();
-  }
-
-  function testCorrectness_WhenUserDepositETH_TokenShouldSafeTransferFromUserToMM() external {
-    vm.prank(ALICE);
-    lendFacet.depositETH{ value: 10 ether }();
-
-    assertEq(nativeToken.balanceOf(ALICE), 0 ether);
-    assertEq(ALICE.balance, 990 ether);
-    assertEq(nativeToken.balanceOf(moneyMarketDiamond), 10 ether);
-
-    assertEq(ibWNative.balanceOf(ALICE), 10 ether);
   }
 
   function testCorrectness_WhenUserWithdrawETH_ShareShouldBurnedAndTransferTokenBackCorrectly() external {
