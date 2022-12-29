5be7e537f1c7b8cf5d6b0bd266d745fb2815acaa
sora-alpaca <sora.alpaca@gmail.com>
2022-12-29 16:41:49 +0700

    test: add rvert test cases

---
+++ solidity/tests/automated-vault/AV_Init.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import { AV_BaseTest } from "./AV_BaseTest.t.sol";
+import { AVDiamondDeployer } from "../helper/AVDiamondDeployer.sol";
+
+import { DiamondCutFacet, IDiamondCut } from "../../contracts/automated-vault/facets/DiamondCutFacet.sol";
+import { DiamondInit } from "../../contracts/automated-vault/initializers/DiamondInit.sol";
+import { AVInit } from "../../contracts/automated-vault/initializers/AVInit.sol";
+
+contract AV_Init is AV_BaseTest {
+  function setUp() public override {
+    super.setUp();
+  }
+
+  function testRevert_WhenInitAVTwice() external {
+    // Deploy DiamondInit
+    AVInit _initializer = new AVInit();
+    IDiamondCut.FacetCut[] memory facetCuts = new IDiamondCut.FacetCut[](0);
+
+    vm.expectRevert(abi.encodeWithSelector(AVInit.AVInit_Initialized.selector));
+    // make lib diamond call init
+    DiamondCutFacet(avDiamond).diamondCut(
+      facetCuts,
+      address(_initializer),
+      abi.encodeWithSelector(bytes4(keccak256("init()")))
+    );
+  }
+}
---
+++ solidity/tests/money-market/MoneyMarket_Init.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import { MoneyMarket_BaseTest } from "./MoneyMarket_BaseTest.t.sol";
+import { MMDiamondDeployer } from "../helper/MMDiamondDeployer.sol";
+
+import { DiamondCutFacet, IDiamondCut } from "../../contracts/money-market/facets/DiamondCutFacet.sol";
+import { DiamondInit } from "../../contracts/money-market/initializers/DiamondInit.sol";
+import { MoneyMarketInit } from "../../contracts/money-market/initializers/MoneyMarketInit.sol";
+
+contract MoneyMarket_Init is MoneyMarket_BaseTest {
+  function setUp() public override {
+    super.setUp();
+  }
+
+  function testRevert_WhenInitDiamondTwice() external {
+    // Deploy DiamondInit
+    DiamondInit diamondInitializer = new DiamondInit();
+    IDiamondCut.FacetCut[] memory facetCuts = new IDiamondCut.FacetCut[](0);
+
+    vm.expectRevert(abi.encodeWithSelector(DiamondInit.DiamondInit_Initialized.selector));
+    // make lib diamond call init
+    DiamondCutFacet(moneyMarketDiamond).diamondCut(
+      facetCuts,
+      address(diamondInitializer),
+      abi.encodeWithSelector(bytes4(keccak256("init()")))
+    );
+  }
+
+  function testRevert_WhenInitMoneyMarketTwice() external {
+    MoneyMarketInit _initializer = new MoneyMarketInit();
+    IDiamondCut.FacetCut[] memory facetCuts = new IDiamondCut.FacetCut[](0);
+
+    vm.expectRevert(abi.encodeWithSelector(MoneyMarketInit.MoneyMarketInit_Initialized.selector));
+    // make lib diamond call init
+    DiamondCutFacet(moneyMarketDiamond).diamondCut(
+      facetCuts,
+      address(_initializer),
+      abi.encodeWithSelector(bytes4(keccak256("init(address,address)")), address(nativeToken), address(nativeRelayer))
+    );
+  }
+}
