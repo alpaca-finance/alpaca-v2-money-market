594cfa6ad27084263a81ccb9e20501b3501b488a
alpanaca <rblue4594@gmail.com>
2022-12-29 13:22:21 +0700

    chore: add doc for borrow facet

--- solidity/contracts/money-market/facets/BorrowFacet.sol
+++ solidity/contracts/money-market/facets/BorrowFacet.sol
@@ -15,6 +15,7 @@ import { LibSafeToken } from "../libraries/LibSafeToken.sol";
 import { IBorrowFacet } from "../interfaces/IBorrowFacet.sol";
 import { IERC20 } from "../interfaces/IERC20.sol";
 
+/// @title BorrowFacet is dedicated to over collateralized borrowing and repayment
 contract BorrowFacet is IBorrowFacet {
   using LibSafeToken for IERC20;
   using LibDoublyLinkedList for LibDoublyLinkedList.List;
@@ -42,6 +43,10 @@ contract BorrowFacet is IBorrowFacet {
     LibReentrancyGuard.unlock();
   }
 
+  /// @notice Borrow a token agaist the placed collaterals
+  /// @param _subAccountId An index to derive the subaccount
+  /// @param _token The token to borrow
+  /// @param _amount The amount to borrow
   function borrow(
     uint256 _subAccountId,
     address _token,
@@ -66,6 +71,11 @@ contract BorrowFacet is IBorrowFacet {
     emit LogBorrow(_subAccount, _token, _amount, _debtShare);
   }
 
+  /// @notice Repay the debt for the subaccount
+  /// @param _account The account to repay for
+  /// @param _subAccountId An index to derive the subaccount
+  /// @param _token The token to repay
+  /// @param _debtShareToRepay The share amount of debt token to repay
   function repay(
     address _account,
     uint256 _subAccountId,
@@ -95,6 +105,11 @@ contract BorrowFacet is IBorrowFacet {
     emit LogRepay(_account, _subAccountId, _token, _actualRepayAmount);
   }
 
+  /// @notice Repay the debt for the subaccount using the same collateral
+  /// @param _account The account to repay for
+  /// @param _subAccountId An index to derive the subaccount
+  /// @param _token The token to repay
+  /// @param _repayAmount The amount to repay
   function repayWithCollat(
     address _account,
     uint256 _subAccountId,
