f419f239ee1867058d80b33b42abd13189d2ee8d
69alpacas <>
2023-01-05 16:37:34 +0700

    feat: add tests, split file

--- solidity/tests/money-market/liquidation-strategies/PancakeswapV2LiquidationStrategy_ExecuteLiquidation.t.sol
+++ solidity/tests/money-market/liquidation-strategies/PancakeswapV2LiquidationStrategy_ExecuteLiquidation.t.sol
@@ -3,10 +3,12 @@ pragma solidity 0.8.17;
 
 import { MoneyMarket_BaseTest, console } from "../MoneyMarket_BaseTest.t.sol";
 import { PancakeswapV2LiquidationStrategy } from "../../../contracts/money-market/PancakeswapV2LiquidationStrategy.sol";
+
+// mocks
 import { MockRouter } from "../../mocks/MockRouter.sol";
 import { MockLPToken } from "../../mocks/MockLPToken.sol";
 
-contract PancakeswapV2LiquidationStrategy_Test is MoneyMarket_BaseTest {
+contract PancakeswapV2LiquidationStrategy_ExecuteLiquidation is MoneyMarket_BaseTest {
   MockLPToken internal wethUsdcLPToken;
   MockRouter internal router;
   PancakeswapV2LiquidationStrategy internal liquidationStrat;
@@ -22,6 +24,13 @@ contract PancakeswapV2LiquidationStrategy_Test is MoneyMarket_BaseTest {
     _callers[0] = address(moneyMarketDiamond);
 
     liquidationStrat.setCallersOk(_callers, true);
+
+    address[][] memory _paths = new address[][](1);
+    _paths[0] = new address[](2);
+    _paths[0][0] = address(weth);
+    _paths[0][1] = address(usdc);
+
+    liquidationStrat.setPaths(_paths);
   }
 
   function testCorrectness_LiquidationStrat_WhenExecuteLiquidation_ShouldWork() external {
@@ -31,12 +40,8 @@ contract PancakeswapV2LiquidationStrategy_Test is MoneyMarket_BaseTest {
     weth.mint(address(liquidationStrat), 1 ether);
     usdc.mint(address(router), 1 ether);
 
-    address[] memory _path = new address[](2);
-    _path[0] = _collatToken;
-    _path[1] = _debtToken;
-
     vm.prank(address(moneyMarketDiamond));
-    liquidationStrat.executeLiquidation(_collatToken, _debtToken, 1 ether, 1 ether, abi.encode(_path));
+    liquidationStrat.executeLiquidation(_collatToken, _debtToken, 1 ether, 1 ether, abi.encode(0));
 
     // nothing left in strat
     assertEq(weth.balanceOf(address(liquidationStrat)), 0);
@@ -59,12 +64,8 @@ contract PancakeswapV2LiquidationStrategy_Test is MoneyMarket_BaseTest {
     weth.mint(address(liquidationStrat), _collatAmount + _injectAmount);
     usdc.mint(address(router), 1 ether);
 
-    address[] memory _path = new address[](2);
-    _path[0] = _collatToken;
-    _path[1] = _debtToken;
-
     vm.prank(address(moneyMarketDiamond));
-    liquidationStrat.executeLiquidation(_collatToken, _debtToken, _collatAmount, _repayAmount, abi.encode(_path));
+    liquidationStrat.executeLiquidation(_collatToken, _debtToken, _collatAmount, _repayAmount, abi.encode(0));
 
     // injected collat left in strat
     assertEq(weth.balanceOf(address(liquidationStrat)), _injectAmount);
@@ -74,26 +75,30 @@ contract PancakeswapV2LiquidationStrategy_Test is MoneyMarket_BaseTest {
     assertEq(usdc.balanceOf(address(router)), 0);
   }
 
-  function testCorrectness_WhenOwnerSetCallersOk_ShouldWork() external {
-    address[] memory _callers = new address[](1);
-    _callers[0] = EVE;
-
-    liquidationStrat.setCallersOk(_callers, true);
-
-    assertTrue(liquidationStrat.callersOk(EVE));
-  }
-
   function testRevert_WhenNotOkCallersCallExecuteLiquidation_ShouldRevert() external {
     address _collatToken = address(weth);
     address _debtToken = address(usdc);
 
-    address[] memory _path = new address[](2);
-    _path[0] = _collatToken;
-    _path[1] = _debtToken;
-
     vm.expectRevert(
       abi.encodeWithSelector(PancakeswapV2LiquidationStrategy.PancakeswapV2LiquidationStrategy_Unauthorized.selector)
     );
-    liquidationStrat.executeLiquidation(_collatToken, _debtToken, 1 ether, 1 ether, abi.encode(_path));
+    liquidationStrat.executeLiquidation(_collatToken, _debtToken, 1 ether, 1 ether, abi.encode(0));
   }
+
+  function testRevert_WhenExecuteLiquidationOnNonExistentPath() external {
+    address _collatToken = address(usdc);
+    address _debtToken = address(weth);
+
+    vm.prank(address(moneyMarketDiamond));
+    vm.expectRevert(
+      abi.encodeWithSelector(
+        PancakeswapV2LiquidationStrategy.PancakeswapV2LiquidationStrategy_PathConfigNotFound.selector,
+        _collatToken,
+        _debtToken
+      )
+    );
+    liquidationStrat.executeLiquidation(_collatToken, _debtToken, 1 ether, 1 ether, abi.encode(0));
+  }
+
+  // TODO: multi-hop integration test with real router
 }
