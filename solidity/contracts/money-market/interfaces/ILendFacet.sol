7e3ef282a2b8f663a13bfa77b1d05a6100cd97de
jr-alpaca <uviveramrom@gmail.com>
2023-01-19 14:53:45 +0700

    chore: refactor mm

--- solidity/contracts/money-market/interfaces/ILendFacet.sol
+++ solidity/contracts/money-market/interfaces/ILendFacet.sol
@@ -2,6 +2,10 @@
 pragma solidity 0.8.17;
 
 interface ILendFacet {
+  error LendFacet_InvalidToken(address _token);
+  error LendFacet_InvalidAddress(address _addr);
+  error LendFacet_InvalidAmount(uint256 _amount);
+
   function deposit(address _token, uint256 _amount) external;
 
   function withdraw(address _ibToken, uint256 _shareAmount) external returns (uint256 _shareValue);
@@ -9,8 +13,4 @@ interface ILendFacet {
   function depositETH() external payable;
 
   function withdrawETH(uint256 _shareAmount) external;
-
-  error LendFacet_InvalidToken(address _token);
-  error LendFacet_InvalidAddress(address _addr);
-  error LendFacet_InvalidAmount(uint256 _amount);
 }
--- solidity/contracts/money-market/interfaces/ILiquidationFacet.sol
+++ solidity/contracts/money-market/interfaces/ILiquidationFacet.sol
@@ -2,26 +2,6 @@
 pragma solidity 0.8.17;
 
 interface ILiquidationFacet {
-  event LogRepurchase(
-    address indexed repurchaser,
-    address _repayToken,
-    address _collatToken,
-    uint256 _actualRepayAmountWithoutFee,
-    uint256 _collatAmountOut,
-    uint256 _feeToTreasury,
-    uint256 _repurchaseRewardToCaller
-  );
-  event LogLiquidate(
-    address indexed caller,
-    address indexed liquidationStrategy,
-    address _repayToken,
-    address _collatToken,
-    uint256 _amountDebtRepaid,
-    uint256 _amountCollatLiquidated,
-    uint256 _feeToTreasury,
-    uint256 _feeToLiquidator
-  );
-
   error LiquidationFacet_Unauthorized();
   error LiquidationFacet_Healthy();
   error LiquidationFacet_RepayAmountExceedThreshold();
