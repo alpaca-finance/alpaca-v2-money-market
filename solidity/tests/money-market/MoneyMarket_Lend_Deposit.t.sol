76a17060d57d599b05c0e237349680070edcbe98
sora-alpaca <sora.alpaca@gmail.com>
2023-01-31 14:05:53 +0700

    test: fix MM lend test

--- solidity/tests/money-market/MoneyMarket_Lend_Deposit.t.sol
+++ solidity/tests/money-market/MoneyMarket_Lend_Deposit.t.sol
@@ -100,7 +100,7 @@ contract MoneyMarket_Lend_DepositTest is MoneyMarket_BaseTest {
     assertEq(ibWeth.balanceOf(ALICE), 10 ether);
 
     vm.startPrank(BOB);
-    collateralFacet.addCollateral(BOB, subAccount0, address(usdc), 20 ether);
+    collateralFacet.addCollateral(BOB, subAccount0, address(usdc), normalizeEther(20 ether, usdcDecimal));
     borrowFacet.borrow(subAccount0, address(weth), 5 ether);
     vm.stopPrank();
 
