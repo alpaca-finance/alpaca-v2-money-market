62660b6293646c5f44932308892772f038bfa2c0
69alpacas <>
2023-02-07 14:02:19 +0700

    feat: setup mm for test script

--- solidity/scripts/utilities/BaseUtilsScript.sol
+++ solidity/scripts/utilities/BaseUtilsScript.sol
@@ -10,36 +10,41 @@ import { LibMoneyMarketDeployment } from "../deployments/libraries/LibMoneyMarke
 // interfaces
 import { IAdminFacet } from "solidity/contracts/money-market/interfaces/IAdminFacet.sol";
 import { IViewFacet } from "solidity/contracts/money-market/interfaces/IViewFacet.sol";
+import { ICollateralFacet } from "solidity/contracts/money-market/interfaces/ICollateralFacet.sol";
+import { IBorrowFacet } from "solidity/contracts/money-market/interfaces/IBorrowFacet.sol";
+import { ILendFacet } from "solidity/contracts/money-market/interfaces/ILendFacet.sol";
 
 // mocks
 import { MockERC20 } from "solidity/tests/mocks/MockERC20.sol";
 
-interface IMoneyMarket is IAdminFacet, IViewFacet {}
+interface IMoneyMarket is IAdminFacet, IViewFacet, ICollateralFacet, IBorrowFacet, ILendFacet {}
 
 abstract contract BaseUtilsScript is Script {
   using stdJson for string;
 
   uint256 internal deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+  uint256 internal userPrivateKey = vm.envUint("USER_PRIVATE_KEY");
   string internal configFilePath =
     string.concat(vm.projectRoot(), string.concat("/configs/", vm.envString("DEPLOYMENT_CONFIG_FILENAME")));
 
   IMoneyMarket internal moneyMarket;
   address internal deployerAddress;
+  address internal userAddress;
 
   struct MoneyMarketConfig {
     address MoneyMarketDiamond;
   }
 
-  function run() public {
-    _setUp();
-    _run();
-  }
+  // function run() public {
+  //   _setUp();
+  //   _run();
+  // }
 
   /// @dev to run script locally pass flag --sig "runLocal()"
-  function runLocal() public {
-    _setUpForLocalRun();
-    _run();
-  }
+  // function runLocal() public {
+  //   _setUpForLocalRun();
+  //   _run();
+  // }
 
   //
   // setups
@@ -50,8 +55,10 @@ abstract contract BaseUtilsScript is Script {
     MoneyMarketConfig memory mmConfig = _getConfig();
     moneyMarket = IMoneyMarket(mmConfig.MoneyMarketDiamond);
     deployerAddress = vm.addr(deployerPrivateKey);
+    userAddress = vm.addr(userPrivateKey);
     console.log("addresses");
     console.log("  deployer     :", deployerAddress);
+    console.log("  user     :", userAddress);
     console.log("  money market :", address(moneyMarket));
   }
 
@@ -63,8 +70,10 @@ abstract contract BaseUtilsScript is Script {
     _stopBroadcast();
     moneyMarket = IMoneyMarket(_moneyMarketDiamond);
     deployerAddress = vm.addr(deployerPrivateKey);
+    userAddress = vm.addr(userPrivateKey);
     console.log("addresses");
     console.log("  deployer     :", deployerAddress);
+    console.log("  user     :", userAddress);
     console.log("  money market :", address(moneyMarket));
   }
 
@@ -88,10 +97,16 @@ abstract contract BaseUtilsScript is Script {
     console.log("==== start broadcast as deployer ====");
   }
 
+  function _startUserBroadcast() internal {
+    vm.startBroadcast(userPrivateKey);
+    console.log("");
+    console.log("==== start broadcast as user ====");
+  }
+
   function _stopBroadcast() internal {
     vm.stopBroadcast();
     console.log("==== broadcast stopped ====\n");
   }
 
-  function _run() internal virtual;
+  // function _run() internal virtual;
 }
---
+++ solidity/scripts/utilities/SetUpMMForTest.s.sol
@@ -0,0 +1,119 @@
+// SPDX-License-Identifier: BUSL
+pragma solidity 0.8.17;
+
+import "./BaseUtilsScript.sol";
+
+import { InterestBearingToken } from "solidity/contracts/money-market/InterestBearingToken.sol";
+import { LibMoneyMarket01 } from "solidity/contracts/money-market/libraries/LibMoneyMarket01.sol";
+import { MockAlpacaV2Oracle } from "solidity/tests/mocks/MockAlpacaV2Oracle.sol";
+
+contract SetUpMMForTestScript is BaseUtilsScript {
+  using stdJson for string;
+
+  function run() public {
+    _setUpForLocalRun();
+
+    _startDeployerBroadcast();
+
+    //---- setup tokens ----//
+    address bnb = address(new MockERC20("", "MOCKBNB", 18));
+    address busd = address(new MockERC20("", "MOCKBUSD", 18));
+    address dodo = address(new MockERC20("", "MOCKDODO", 18));
+    address pstake = address(new MockERC20("", "MOCKPSTAKE", 18));
+    address mock6DecimalsToken = address(new MockERC20("", "MOCK6", 6));
+
+    //---- setup mock oracle ----//
+    MockAlpacaV2Oracle mockOracle = new MockAlpacaV2Oracle();
+    mockOracle.setTokenPrice(bnb, 1 ether);
+    mockOracle.setTokenPrice(busd, 1 ether);
+    mockOracle.setTokenPrice(dodo, 1 ether);
+    mockOracle.setTokenPrice(pstake, 1 ether);
+    mockOracle.setTokenPrice(mock6DecimalsToken, 1 ether);
+
+    moneyMarket.setOracle(address(mockOracle));
+
+    //---- setup mm configs ----//
+    moneyMarket.setMinDebtSize(0.1 ether);
+    moneyMarket.setMaxNumOfToken(3, 3, 3);
+
+    moneyMarket.setIbTokenImplementation(address(new InterestBearingToken()));
+
+    //---- open markets ----//
+    IAdminFacet.TokenConfigInput memory tokenConfigInput = IAdminFacet.TokenConfigInput({
+      token: bnb,
+      tier: LibMoneyMarket01.AssetTier.COLLATERAL,
+      collateralFactor: 9000,
+      borrowingFactor: 9000,
+      maxBorrow: 30 ether,
+      maxCollateral: 100 ether
+    });
+    address ibBnb = moneyMarket.openMarket(bnb, tokenConfigInput, tokenConfigInput);
+    tokenConfigInput.token = busd;
+    address ibBusd = moneyMarket.openMarket(busd, tokenConfigInput, tokenConfigInput);
+    tokenConfigInput.token = mock6DecimalsToken;
+    address ibMock6 = moneyMarket.openMarket(mock6DecimalsToken, tokenConfigInput, tokenConfigInput);
+    console.log("openMarket for", mock6DecimalsToken);
+    tokenConfigInput.token = dodo;
+    tokenConfigInput.tier = LibMoneyMarket01.AssetTier.CROSS;
+    tokenConfigInput.collateralFactor = 0;
+    tokenConfigInput.maxCollateral = 0;
+    address ibDodo = moneyMarket.openMarket(dodo, tokenConfigInput, tokenConfigInput);
+    tokenConfigInput.token = pstake;
+    tokenConfigInput.tier = LibMoneyMarket01.AssetTier.ISOLATE;
+    address ibPstake = moneyMarket.openMarket(pstake, tokenConfigInput, tokenConfigInput);
+
+    _stopBroadcast();
+
+    //---- setup user positions ----//
+    MockERC20(bnb).mint(userAddress, 100 ether);
+    MockERC20(busd).mint(userAddress, 100 ether);
+    MockERC20(mock6DecimalsToken).mint(userAddress, 100e6);
+    MockERC20(dodo).mint(userAddress, 100 ether);
+    MockERC20(pstake).mint(userAddress, 100 ether);
+
+    _startUserBroadcast();
+
+    MockERC20(bnb).approve(address(moneyMarket), type(uint256).max);
+    MockERC20(busd).approve(address(moneyMarket), type(uint256).max);
+    MockERC20(mock6DecimalsToken).approve(address(moneyMarket), type(uint256).max);
+    MockERC20(dodo).approve(address(moneyMarket), type(uint256).max);
+    MockERC20(pstake).approve(address(moneyMarket), type(uint256).max);
+
+    // seed money market
+    moneyMarket.deposit(dodo, 10 ether);
+    moneyMarket.deposit(pstake, 10 ether);
+    moneyMarket.deposit(mock6DecimalsToken, 10e6);
+
+    // subAccount 0
+    moneyMarket.addCollateral(userAddress, 0, bnb, 1 ether);
+    moneyMarket.addCollateral(userAddress, 0, busd, 10 ether);
+
+    moneyMarket.borrow(0, dodo, 1 ether);
+    moneyMarket.borrow(0, mock6DecimalsToken, 1e6);
+
+    // subAccount 1
+    moneyMarket.addCollateral(userAddress, 1, mock6DecimalsToken, 10e6);
+
+    moneyMarket.borrow(1, pstake, 1 ether);
+
+    _stopBroadcast();
+
+    //---- write deployed addresses ----//
+
+    console.log("write output to", configFilePath);
+    string memory configJson;
+    configJson.serialize("ibBnb", ibBnb);
+    configJson.serialize("ibBusd", ibBusd);
+    configJson.serialize("ibDodo", ibDodo);
+    configJson.serialize("ibPstake", ibPstake);
+    configJson = configJson.serialize("ibMock6", ibMock6);
+    configJson.write(configFilePath, ".IbTokens");
+
+    configJson.serialize("bnb", bnb);
+    configJson.serialize("busd", busd);
+    configJson.serialize("dodo", dodo);
+    configJson.serialize("pstake", pstake);
+    configJson = configJson.serialize("mock6DecimalsToken", mock6DecimalsToken);
+    configJson.write(configFilePath, ".Tokens");
+  }
+}
