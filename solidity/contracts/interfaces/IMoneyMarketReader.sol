12e5d5c73e5d338c480d2b842fdf661b04c83974
wow-alpaca <wowza.alpaca@gmail.com>
2023-03-10 03:10:35 +0700

    feat: reward reader

--- solidity/contracts/interfaces/IMoneyMarketReader.sol
+++ solidity/contracts/interfaces/IMoneyMarketReader.sol
@@ -72,6 +72,13 @@ interface IMoneyMarketReader {
     SupplyAccountDetail[] supplyAccountDetails;
   }
 
+  struct RewardSummary {
+    uint256 ibPoolId;
+    uint256 debtPoolId;
+    uint256 lendingPendingReward;
+    uint256 borrowingPendingReward;
+  }
+
   function getMarketSummary(address _underlyingToken) external view returns (MarketSummary memory);
 
   function getSubAccountSummary(address _account, uint256 _subAccountId)
