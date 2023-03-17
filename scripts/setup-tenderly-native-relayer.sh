6b38843795bcca08ce0418ae614fd455b5282cad
wow-alpaca <wowza.alpaca@gmail.com>
2023-03-17 19:10:36 +0700

    chore: add rpc url

--- scripts/setup-tenderly-native-relayer.sh
+++ scripts/setup-tenderly-native-relayer.sh
@@ -2,9 +2,13 @@
 
 source .env
 
+# # build tenderly rpc url
+TENDERLY_RPC_URL="https://rpc.tenderly.co/fork/$TENDERLY_FORK_ID"
+echo $TENDERLY_RPC_URL
+
 # set account manager as caller ok of native relayer
 NATIVE_RELAYER=0xE1D2CA01bc88F325fF7266DD2165944f3CAf0D3D
-RELAYER_OWNER=$(cast call $NATIVE_RELAYER "owner()" | cut -c 1-2,27-66)
+RELAYER_OWNER=$(cast call $NATIVE_RELAYER --rpc-url $TENDERLY_RPC_URL "owner()" | cut -c 1-2,27-66)
 curl --location --request POST $TENDERLY_RPC_URL \
     --header 'Content-Type: application/json' \
     --data-raw '{
@@ -16,4 +20,4 @@ curl --location --request POST $TENDERLY_RPC_URL \
     ],
     "id": "1234"
 }'
-cast send $NATIVE_RELAYER --from $RELAYER_OWNER "setCallerOk(address[],bool)" "[$LOCAL_ACCOUNT_MANAGER]" true
+cast send $NATIVE_RELAYER --rpc-url $TENDERLY_RPC_URL --from $RELAYER_OWNER "setCallerOk(address[],bool)" "[$LOCAL_ACCOUNT_MANAGER]" true
