9f35cef546b9aeaf5cc58a83273e78ec9d2bcbd6
69alpacas <alpaca69.dev@gmail.com>
2023-04-11 11:53:59 +0700

    chore: cleanup

--- script/utilities/CreateLiquidationScenario.s.sol
+++ script/utilities/CreateLiquidationScenario.s.sol
@@ -3,8 +3,6 @@ pragma solidity 0.8.19;
 
 import "../BaseScript.sol";
 
-// import { SimplePriceOracle } from "solidity/contracts/oracle/SimplePriceOracle.sol";
-// import { TransparentUpgradeableProxy } from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import { MockAlpacaV2Oracle } from "solidity/tests/mocks/MockAlpacaV2Oracle.sol";
 
 contract CreateLiquidationScenarioScript is BaseScript {
@@ -14,22 +12,6 @@ contract CreateLiquidationScenarioScript is BaseScript {
     _loadAddresses();
 
     _startDeployerBroadcast();
-
-    // address implementation = address(new SimplePriceOracle());
-    // bytes memory data = abi.encodeWithSelector(bytes4(keccak256("initialize(address)")), deployerAddress);
-    // SimplePriceOracle oracle = SimplePriceOracle(
-    //   address(new TransparentUpgradeableProxy(implementation, proxyAdminAddress, data))
-    // );
-
-    // address[] memory token0s = new address[](1);
-    // token0s[0] = wbnb;
-    // address[] memory token1s = new address[](1);
-    // token1s[0] = usdPlaceholder;
-    // uint256[] memory prices = new uint256[](1);
-    // prices[0] = 500 ether; // current bnb @311
-    // oracle.setPrices(token0s, token1s, prices);
-
-    // alpacaV2Oracle.setOracle(address(oracle));
 
     MockAlpacaV2Oracle oracle = new MockAlpacaV2Oracle();
     oracle.setTokenPrice(wbnb, 700 ether);
