8cea50d303f6a53999f20af52889216cb420c126
jr-alpaca <uviveramrom@gmail.com>
2023-04-23 22:39:31 +0700

    chore: refactor setTokenConfig

--- script/deployments/AlpacaV2Oracle/config/SetTokenConfig.s.sol
+++ script/deployments/AlpacaV2Oracle/config/SetTokenConfig.s.sol
@@ -3,11 +3,22 @@ pragma solidity 0.8.19;
 
 import "../../../BaseScript.sol";
 
-import { AlpacaV2Oracle } from "solidity/contracts/oracle/AlpacaV2Oracle.sol";
 import { IAlpacaV2Oracle } from "solidity/contracts/oracle/interfaces/IAlpacaV2Oracle.sol";
 
 contract SetTokenConfigScript is BaseScript {
   using stdJson for string;
+
+  address[] tokens;
+  IAlpacaV2Oracle.Config[] configs;
+
+  struct SetTokenConfigInput {
+    address[] path;
+    address router;
+    uint64 maxPriceDiffBps;
+    bool isUsingV3Pool;
+  }
+
+  address[] tokenPath;
 
   function run() public {
     /*
@@ -21,40 +32,30 @@ contract SetTokenConfigScript is BaseScript {
     */
 
     // set alpaca guard path
-    uint8 configLength = 2;
-    address[] memory tokens = new address[](configLength);
-    IAlpacaV2Oracle.Config[] memory configs = new IAlpacaV2Oracle.Config[](configLength);
+    address[] memory _path;
 
-    address[] memory path;
-    // CAKE
-    path = new address[](2);
-    path[0] = cake;
-    path[1] = busd;
-
-    configs[0] = IAlpacaV2Oracle.Config({
-      router: pancakeswapV2Router,
-      maxPriceDiffBps: 10500,
-      path: path,
-      isUsingV3Pool: false
-    });
-
-    // DOT
-    path = new address[](3);
-    path[0] = dot;
-    path[1] = wbnb;
-    path[2] = busd;
-
-    tokens[1] = dot;
-    configs[1] = IAlpacaV2Oracle.Config({
-      router: pancakeswapV2Router,
-      maxPriceDiffBps: 10500,
-      path: path,
-      isUsingV3Pool: false
-    });
+    _path = new address[](2);
+    _path[0] = cake;
+    _path[1] = busd;
+    addSetTokenConfigList(
+      SetTokenConfigInput({ path: _path, router: pancakeswapV2Router, maxPriceDiffBps: 10500, isUsingV3Pool: false })
+    );
 
     //---- execution ----//
     _startDeployerBroadcast();
     alpacaV2Oracle.setTokenConfig(tokens, configs);
     _stopBroadcast();
+  }
+
+  function addSetTokenConfigList(SetTokenConfigInput memory _input) internal {
+    IAlpacaV2Oracle.Config memory config = IAlpacaV2Oracle.Config({
+      router: _input.router,
+      maxPriceDiffBps: _input.maxPriceDiffBps,
+      path: _input.path,
+      isUsingV3Pool: _input.isUsingV3Pool
+    });
+
+    tokens.push(_input.path[0]);
+    configs.push(config);
   }
 }
