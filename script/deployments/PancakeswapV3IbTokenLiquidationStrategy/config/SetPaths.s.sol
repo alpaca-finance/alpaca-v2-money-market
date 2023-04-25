86c5f4bf24e889ec7b80f8ebeef38639393dd603
jr-alpaca <uviveramrom@gmail.com>
2023-04-25 22:58:03 +0700

    chore: PCSV3 ib SetPath script

--- script/deployments/PancakeswapV3IbTokenLiquidationStrategy/config/SetPaths.s.sol
+++ script/deployments/PancakeswapV3IbTokenLiquidationStrategy/config/SetPaths.s.sol
@@ -25,11 +25,106 @@ contract SetPathsScript is BaseScript {
     _strats[0] = address(pancakeswapV3IbLiquidateStrat);
     bytes memory liquidationPath;
 
-    liquidationPath = encodePool(wbnb, 500, busd);
+    // ********* WBNB ********* //
+    // WBNB -> USDC:
+    liquidationPath = encodePath(wbnb, 500, usdt, 100, usdc);
+    setLiquidationPath(liquidationPath);
+    // WBNB -> USDT:
+    liquidationPath = encodePath(wbnb, 500, usdt);
+    setLiquidationPath(liquidationPath);
+    // WBNB -> BUSD:
+    liquidationPath = encodePath(wbnb, 500, busd);
+    setLiquidationPath(liquidationPath);
+    // WBNB -> BTCB:
+    liquidationPath = encodePath(wbnb, 2500, btcb);
+    setLiquidationPath(liquidationPath);
+    // WBNB -> ETH:
+    liquidationPath = encodePath(wbnb, 2500, eth);
     setLiquidationPath(liquidationPath);
 
-    liquidationPath = encodePool(dot, 2500, wbnb);
-    addPath(liquidationPath, 500, busd);
+    // ********* USDC ********* //
+    // USDC -> BTCB:
+    liquidationPath = encodePath(usdc, 100, busd, 500, btcb);
+    setLiquidationPath(liquidationPath);
+    // USDC -> ETH:
+    liquidationPath = encodePath(usdc, 500, eth);
+    setLiquidationPath(liquidationPath);
+    // USDC -> WBNB:
+    liquidationPath = encodePath(usdc, 100, usdt, 500, wbnb);
+    setLiquidationPath(liquidationPath);
+    // USDC -> BUSD:
+    liquidationPath = encodePath(usdc, 100, busd);
+    setLiquidationPath(liquidationPath);
+    // USDC -> USDT:
+    liquidationPath = encodePath(usdc, 100, usdt);
+    setLiquidationPath(liquidationPath);
+
+    // ********* USDT ********* //
+    // USDT -> BTCB:
+    liquidationPath = encodePath(usdt, 500, btcb);
+    setLiquidationPath(liquidationPath);
+    // USDT -> ETH:
+    liquidationPath = encodePath(usdt, 500, wbnb, 2500, eth);
+    setLiquidationPath(liquidationPath);
+    // USDT -> WBNB:
+    liquidationPath = encodePath(usdt, 500, wbnb);
+    setLiquidationPath(liquidationPath);
+    // USDT -> BUSD:
+    liquidationPath = encodePath(usdt, 100, busd);
+    setLiquidationPath(liquidationPath);
+    // USDT -> USDC:
+    liquidationPath = encodePath(usdt, 100, usdc);
+    setLiquidationPath(liquidationPath);
+
+    // ********* BUSD ********* //
+    // BUSD -> BTCB:
+    liquidationPath = encodePath(busd, 500, btcb);
+    setLiquidationPath(liquidationPath);
+    // BUSD -> ETH:
+    liquidationPath = encodePath(busd, 500, wbnb, 2500, eth);
+    setLiquidationPath(liquidationPath);
+    // BUSD -> WBNB:
+    liquidationPath = encodePath(busd, 500, wbnb);
+    setLiquidationPath(liquidationPath);
+    // BUSD -> USDT:
+    liquidationPath = encodePath(busd, 100, usdt);
+    setLiquidationPath(liquidationPath);
+    // BUSD -> USDC:
+    liquidationPath = encodePath(busd, 100, usdc);
+    setLiquidationPath(liquidationPath);
+
+    // ********* BTCB ********* //
+    // BTCB -> ETH
+    liquidationPath = encodePath(btcb, 2500, eth);
+    setLiquidationPath(liquidationPath);
+    // BTCB -> WBNB
+    liquidationPath = encodePath(btcb, 2500, wbnb);
+    setLiquidationPath(liquidationPath);
+    // BTCB -> BUSD
+    liquidationPath = encodePath(btcb, 500, busd);
+    setLiquidationPath(liquidationPath);
+    // BTCB -> USDT
+    liquidationPath = encodePath(btcb, 500, usdt);
+    setLiquidationPath(liquidationPath);
+    // BTCB -> USDC
+    liquidationPath = encodePath(btcb, 500, busd, 100, usdc);
+    setLiquidationPath(liquidationPath);
+
+    // ********* ETH ********* //
+    // ETH -> BTCB
+    liquidationPath = encodePath(eth, 2500, btcb);
+    setLiquidationPath(liquidationPath);
+    // ETH -> WBNB
+    liquidationPath = encodePath(eth, 2500, wbnb);
+    setLiquidationPath(liquidationPath);
+    // ETH -> BUSD
+    liquidationPath = encodePath(eth, 2500, wbnb, 500, busd);
+    setLiquidationPath(liquidationPath);
+    // ETH -> USDT
+    liquidationPath = encodePath(eth, 2500, wbnb, 500, usdt);
+    setLiquidationPath(liquidationPath);
+    // ETH -> USDC
+    liquidationPath = encodePath(eth, 500, usdc);
     setLiquidationPath(liquidationPath);
 
     //---- execution ----//
@@ -42,7 +137,7 @@ contract SetPathsScript is BaseScript {
     _stopBroadcast();
   }
 
-  function encodePool(
+  function encodePath(
     address _tokenA,
     uint24 _fee,
     address _tokenB
@@ -50,12 +145,14 @@ contract SetPathsScript is BaseScript {
     pool = abi.encodePacked(_tokenA, _fee, _tokenB);
   }
 
-  function addPath(
-    bytes memory liquidationPath,
-    uint24 _fee,
-    address _tokenB
-  ) internal pure {
-    liquidationPath = abi.encodePacked(liquidationPath, _fee, _tokenB);
+  function encodePath(
+    address _tokenA,
+    uint24 _fee0,
+    address _tokenB,
+    uint24 _fee1,
+    address _tokenC
+  ) internal pure returns (bytes memory pool) {
+    pool = abi.encodePacked(_tokenA, _fee0, _tokenB, _fee1, _tokenC);
   }
 
   function setLiquidationPath(bytes memory liquidationPath) internal {
