ff7040e81ea87193d162a607dcda0057257a1b2b
jr-alpaca <uviveramrom@gmail.com>
2023-05-09 15:36:11 +0700

    chore: compile typechain

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,32 +1,42 @@
+import fs from "fs";
+import { HardhatUserConfig } from "hardhat/config";
 import { config as dotEnvConfig } from "dotenv";
 dotEnvConfig();
 
 import "@openzeppelin/hardhat-upgrades";
 import "@nomiclabs/hardhat-ethers";
 import "@typechain/hardhat";
+import "hardhat-deploy";
+
+function getRemappings() {
+  return fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean)
+    .map((line) => line.trim().split("="));
+}
 
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
-    // rinkeby: {
-    //   url: process.env.RINKEBY_RPC,
-    //   accounts:
-    //     process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    // },
+    mainnet: {
+      chainId: 56,
+      url: process.env.BSC_RPC_URL,
+      accounts: process.env.DEPLOYER_PRIVATE_KEY !== undefined ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+    },
   },
   solidity: {
-    version: "0.8.13",
+    version: "0.8.19",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
+        runs: 1,
       },
-      evmVersion: "istanbul",
     },
   },
   paths: {
-    sources: "./contracts",
-    tests: "./test",
+    sources: "./solidity/contracts",
+    tests: "./tests",
     cache: "./cache",
     artifacts: "./artifacts",
   },
@@ -34,7 +44,19 @@ module.exports = {
     outDir: "./typechain",
     target: process.env.TYPECHAIN_TARGET || "ethers-v5",
   },
-  mocha: {
-    timeout: 100000,
+  // This fully resolves paths for imports in the ./lib directory for Hardhat
+  preprocess: {
+    eachLine: (hre: any) => ({
+      transform: (line: string) => {
+        if (line.match(/^\s*import /i)) {
+          getRemappings().forEach(([find, replace]) => {
+            if (line.match(find)) {
+              line = line.replace(find, replace);
+            }
+          });
+        }
+        return line;
+      },
+    }),
   },
 };
