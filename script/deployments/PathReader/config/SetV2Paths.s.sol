455ea398d2b5a8ad69b6cd070f9c99fe691e9cbf
m100alpaca <m100alpaca@gmail.com>
2023-05-18 11:21:44 +0700

    fix: fix script v2 path

--- script/deployments/PathReader/config/SetV2Paths.s.sol
+++ script/deployments/PathReader/config/SetV2Paths.s.sol
@@ -9,6 +9,7 @@ contract SetV2PathsScript is BaseScript {
   using stdJson for string;
 
   IUniSwapV2PathReader.PathParams[] paths;
+  address router;
 
   function run() public {
     /*
@@ -21,25 +22,46 @@ contract SetV2PathsScript is BaseScript {
   Check all variables below before execute the deployment script
     */
 
-    /* Template:
-    {
-      address _router = pancakeswapRouterV2;
-      address[] memory _path = new address[](2);
-      _path[0] = wbnb;
-      _path[1] = busd;
+    // Setup router
+    router = pancakeswapRouterV2;
 
-      _setPath(_path);
-    }
-     */
+    // ********* WBNB ********* //
+    // WBNB -> HIGH:
+    _addLiquidationPath(wbnb, busd, high);
 
-    {
-      address _router = pancakeswapRouterV2;
-      address[] memory _path = new address[](2);
-      _path[0] = wbnb;
-      _path[1] = busd;
+    // ********* USDC ********* //
+    // USDC -> HIGH:
+    _addLiquidationPath(usdc, busd, high);
 
-      _setPath(_router, _path);
-    }
+    // ********* USDT ********* //
+    // USDT -> HIGH:
+    _addLiquidationPath(usdt, busd, high);
+
+    // ********* BUSD ********* //
+    // BUSD -> HIGH:
+    _addLiquidationPath(busd, high);
+
+    // ********* BTCB ********* //
+    // BTCB -> HIGH:
+    _addLiquidationPath(btcb, busd, high);
+
+    // ********* ETH ********* //
+    // ETH -> HIGH:
+    _addLiquidationPath(eth, busd, high);
+
+    // ********* HIGH ********* //
+    // HIGH -> WBNB:
+    _addLiquidationPath(high, busd, wbnb);
+    // HIGH -> USDC:
+    _addLiquidationPath(high, busd, usdc);
+    // HIGH -> USDT:
+    _addLiquidationPath(high, busd, usdt);
+    // HIGH -> BUSD:
+    _addLiquidationPath(high, busd);
+    // HIGH -> BTCB:
+    _addLiquidationPath(high, busd, btcb);
+    // HIGH -> ETH:
+    _addLiquidationPath(high, busd, eth);
 
     _startDeployerBroadcast();
 
@@ -48,10 +70,28 @@ contract SetV2PathsScript is BaseScript {
     _stopBroadcast();
   }
 
-  function _setPath(address _router, address[] memory _path) internal {
-    IUniSwapV2PathReader.PathParams memory _pathParam = IUniSwapV2PathReader.PathParams(_router, _path);
-    paths.push(_pathParam);
+  function _addLiquidationPath(address _token1, address _token2) internal {
+    address[] memory path = new address[](2);
+    path[0] = _token1;
+    path[1] = _token2;
 
-    delete _pathParam;
+    IUniSwapV2PathReader.PathParams memory _input = IUniSwapV2PathReader.PathParams({ router: router, path: path });
+
+    paths.push(_input);
+  }
+
+  function _addLiquidationPath(
+    address _token1,
+    address _token2,
+    address _token3
+  ) internal {
+    address[] memory path = new address[](3);
+    path[0] = _token1;
+    path[1] = _token2;
+    path[2] = _token3;
+
+    IUniSwapV2PathReader.PathParams memory _input = IUniSwapV2PathReader.PathParams({ router: router, path: path });
+
+    paths.push(_input);
   }
 }
