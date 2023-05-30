bbdbeb7f80c92b15619fce85c4b61caa9862c47b
jr-alpaca <uviveramrom@gmail.com>
2023-05-30 15:12:44 +0700

    chore: add slippage control

---
+++ script/deployments/SmartTreasury/config/SetSlippageToleranceBps.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "../../../BaseScript.sol";
+
+contract SetSlippageToleranceBpsScript is BaseScript {
+  using stdJson for string;
+
+  function run() public {
+    /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+    */
+
+    uint16 _slippageToleranceBps = 100;
+
+    _startDeployerBroadcast();
+
+    smartTreasury.setSlippageToleranceBps(_slippageToleranceBps);
+
+    _stopBroadcast();
+  }
+}
