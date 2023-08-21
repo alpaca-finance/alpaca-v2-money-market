1fb42938130b1d015e192ecb6d25b2a2b374768e
jr-alpaca <uviveramrom@gmail.com>
2023-08-21 11:56:59 +0700

    feat: borrowFacet deployment script

---
+++ script/deployments/MoneyMarket/deploy/BorrowFacet.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "../../../BaseScript.sol";
+
+import { BorrowFacet } from "solidity/contracts/money-market/facets/BorrowFacet.sol";
+
+contract DeployLiquidationFacetScript is BaseScript {
+  using stdJson for string;
+
+  function run() public {
+    /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+    */
+
+    _startDeployerBroadcast();
+
+    address borrowFacet = address(new BorrowFacet());
+
+    _stopBroadcast();
+
+    _writeJson(vm.toString(borrowFacet), ".moneyMarket.facets.borrowFacet");
+  }
+}
